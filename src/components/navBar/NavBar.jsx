65a6f76c97d7c422aed14e533c9a309858b1a7a6
Beslin Ebby <beslinebby00@gmail.com>
2025-12-14 21:29:56 +0530

    updated responsive styles for header

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { FaRegUser } from "react-icons/fa6";
 import { FiShoppingCart } from "react-icons/fi";
+import { GiHamburgerMenu } from "react-icons/gi"
+import { IoMdClose } from "react-icons/io";
 import './NavBar.css'
 
 const NavBar = () => {
+
+const [visibleSideBar,setVisibleSideBar]=useState(false)
 
   return (
      <header className="navbar">
@@ -48,7 +52,33 @@ const NavBar = () => {
                  <p>0</p>
               </Link>
            </div>
+           <div className="nav-menu" onClick={() => setVisibleSideBar(true)}>
+              <GiHamburgerMenu fontSize={"20px"} />
+           </div>
         </div>
+        {visibleSideBar && (
+           <div className="sidebar">
+              <div className="back-btn">
+                 <button onClick={() => setVisibleSideBar(false)}>
+                    <IoMdClose fontSize={"20px"}/>
+                 </button>
+              </div>
+              <div className="sidebar-cnt">
+                 <NavLink onClick={() => setVisibleSideBar(false)} to={"/"}>
+                    <p>HOME</p>
+                 </NavLink>
+                 <NavLink onClick={() => setVisibleSideBar(false)} to={"/collection"}>
+                    <p>COLLECTION</p>
+                 </NavLink>
+                 <NavLink onClick={() => setVisibleSideBar(false)} to={"/about"}>
+                    <p>ABOUT</p>
+                 </NavLink>
+                 <NavLink onClick={() => setVisibleSideBar(false)} to={"/contact"}>
+                    <p>CONTACT </p>
+                 </NavLink>
+              </div>
+           </div>
+        )}
      </header>
   );
 }
