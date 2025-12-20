9f54ddac5c8c25a2da294211ceb219eae8da3cd1
Beslin Ebby <beslinebby00@gmail.com>
2025-12-20 21:12:33 +0530

    added functionalities for cart

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -3,28 +3,43 @@ import { createContext, useState } from "react";
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
+   const [cartData, setCartData] = useState([]);
 
-   const [cartProducts, setCartProducts] = useState([]);
-   const [quantity, setQuantity] = useState(1);
+   const shippingFee = 2;
 
-   const addToCart = (productId) => {
-      setCartProducts((prevCart) => {
+   const addToCart = (productId, price) => {
+      setCartData((prevCart) => {
          const existingProduct = prevCart.find((item) => item.id === productId);
 
          if (existingProduct) {
             return prevCart.map((item) =>
-               item.id === productId ? { ...item, quantity: item.quantity + quantity } : item
+               item.id === productId ? { ...item, quantity: item.quantity + 1, total: item.total + price } : item
             );
          }
-
-         return [...prevCart, { id: productId, quantity: quantity }];
+         return [...prevCart, { id: productId, quantity: 1, total: price }];
       });
    };
 
+   const updateQuantity = (productId, newQty, price) => {
+      setCartData((prev) =>
+         prev.map((item) => (item.id === productId ? { ...item, quantity: newQty, total: newQty * price } : item))
+      );
+   };
+
+   const removeProduct = (productId) => {
+      setCartData((prev) => prev.filter((data) => data.id !== productId));
+   };
+
+   const getCartTotal = () => {
+      const cartTotal = cartData.reduce((acc, curr) => acc + curr.total, 0);
+      return Math.round(cartTotal);
+   };
 
    return (
-      <CartContext.Provider value={{ cartProducts, quantity, setQuantity, addToCart }}>{children}</CartContext.Provider>
+      <CartContext.Provider value={{ cartData, addToCart, updateQuantity, removeProduct, getCartTotal, shippingFee }}>
+         {children}
+      </CartContext.Provider>
    );
 };
 
-export default CartProvider
+export default CartProvider;
